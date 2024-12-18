050e0e3dbef642a7b6823077ff94b3c3d045dc18
Benjamin Bigdelle <benjaminbigdelle@gmail.com>
2024-12-18 14:50:06 -0500

    runnabel on render
    Signed-off-by: Benjamin Bigdelle <benjaminbigdelle@gmail.com>

--- project/app.py
+++ project/app.py
@@ -86,5 +86,6 @@ def time_series():
 
     return render_template('time_series.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Get Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)
